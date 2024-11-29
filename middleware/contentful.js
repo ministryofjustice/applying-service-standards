70653459665c78b29fe20e558f7c77d2a5f6bf1b
Emily Hazlehurst <emily.hazlehurst@digital.justice.gov.uk>
2024-11-29 13:53:30 +0000

    Add secrets for deployment

--- middleware/contentful.js
+++ middleware/contentful.js
@@ -1,8 +1,8 @@
 const contentful = require('contentful');
 
 const client = contentful.createClient({
-    space: process.env.spaceID || 'space_id',
-    accessToken: process.env.contentfulLiveAPI || 'contentful_api'
+    space: process.env.CONTENTFUL_SPACE_ID || 'space_id',
+    accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN || 'contentful_api'
 });
 
 module.exports = client;
